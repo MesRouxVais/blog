562405986fa7772dce668f3ec92686d7d40218cc
Mérovée Sohier <sohier.merovee@proton.me>
2024-10-12 15:46:04 +0200

    Initial commit

---
+++ darkModeButton.js
@@ -0,0 +1,10 @@
+document.querySelector('.checkbox').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? 'Mode Clair' : 'Mode Sombre';
+    var themeLink = document.getElementById('theme-link');
+    if (themeLink.getAttribute('href') === 'lighCode.css') {
+        themeLink.setAttribute('href', 'darkCode.css');
+    } else {
+        themeLink.setAttribute('href', 'lighCode.css');
+    }
+});
